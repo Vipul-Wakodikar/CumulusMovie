1cfe1871af2fef3ac683f885abe940ad4553dd7b
Vipul-Wakodikar <wakodikar.vipul@gmail.com>
2021-12-28 22:11:57 +0530

    deployed commit

--- src/Component/MovieCard.js
+++ src/Component/MovieCard.js
@@ -95,7 +95,7 @@ const MovieCard = () => {
                     color: "#FFD700"
                   }}
                 >
-                   <img src = {dataVals.Poster} height = "auto" width = "auto" /> 
+                   <img src = {dataVals.Poster} height = "auto" width = "auto" /> <br />
                   {dataVals.Title}
                 </FrontSide>
                 <BackSide style={{ backgroundColor: "#222",color:"#FFD700" }}>
